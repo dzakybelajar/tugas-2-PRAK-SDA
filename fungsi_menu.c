480d14466d5063259b09fd9eb8b667ac11966ca5
dzakybelajar <dzakysyraj@mhs.usk.ac.id>
2026-05-30 20:14:48 +0700

    tambah fungsi menu

--- fungsi_menu.c
+++ fungsi_menu.c
@@ -7,3 +7,11 @@ void menu_utama(){
     do
     {   
         input = 0;
+        printf("\n==== MENU UTAMA ====\n");
+        printf("1. Sorting Dasar\n");
+        printf("2. Advanced Sorting\n");
+        printf("3. Keluar\n");
+        printf("Pilih Menu: ");
+        scanf("%d", &input);
+        while(getchar() !='\n');
+        switch (input)
