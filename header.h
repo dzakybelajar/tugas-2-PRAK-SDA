560b548c39f2e198b969ed70e225f2475965b680
dzakybelajar <dzakysyraj@mhs.usk.ac.id>
2026-05-30 19:52:15 +0700

    file header

---
+++ header.h
@@ -0,0 +1,7 @@
+#define MAX_INT 1000
+#define MAX_WORD 3000
+void menu_utama();
+void menu_sorting_dasar();
+void menu_advanced_sorting();
+void acak_data(int);
+int InputTampilanSebelumAtauSetelahSorting(int);
